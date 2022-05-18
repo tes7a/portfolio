74aa24f20787ed4f2ab85b035bceb13d687eb053
tes7a <ttes7a@gmail.com>
2022-05-18 23:16:32 +0300

    refactor Header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,11 @@
-import React, {UIEventHandler, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import s from "./Header.module.scss";
 import {Nav} from "../Nav/Nav";
 import eat from "../assets/image/restaurant.svg";
 import sleep from "../assets/image/sleep.svg";
 import code from "../assets/image/programming-code-signs.svg";
 import repeat from "../assets/image/repeat.svg";
+import Particles from "react-particles-js";
 
 export const Header = () => {
     const [scroll, setScroll] = useState(false);
@@ -29,15 +30,39 @@ export const Header = () => {
                 <Nav/>
             </div>
         </div>
+        <div className={s.nameSurnameBlock}>
+            <h1>Konstantine <span>Dudkin</span></h1>
+            <ul className={s.headerSocial}>
+                <li><a href="/#">
+                    <i className={`fa-brands fa-facebook ${s.socialsIcons}`}></i>
+                </a></li>
+                <li><a href="/#">
+                    <i className={`fa-brands fa-instagram ${s.socialsIcons}`}></i>
+                </a></li>
+                <li><a href="/#">
+                    <i className={`fa-brands fa-telegram ${s.socialsIcons}`}></i>
+                </a></li>
+                <li><a href="/#">
+                    <i className={`fa-brands fa-linkedin ${s.socialsIcons}`}></i>
+                </a></li>
+                <li><a href="/#">
+                    <i className={`fa-solid fa-envelope ${s.socialsIcons}`}></i>
+                </a></li>
+            </ul>
+        </div>
         <div className={s.imgContainer}>
-            <img src={eat} alt={"img"}/>
-            <span>eat();</span>
-            <img src={sleep} alt={"img"}/>
-            <span>eat();</span>
-            <img src={code} alt={"img"}/>
-            <span>eat();</span>
-            <img src={repeat} alt={"img"}/>
-            <span>eat();</span>
+            <div className={s.eatBlock}>
+                <img src={eat} alt={"img"}/>
+            </div>
+            <div className={s.sleepBlock}>
+                <img src={sleep} alt={"img"}/>
+            </div>
+            <div className={s.codeBlock}>
+                <img src={code} alt={"img"}/>
+            </div>
+            <div className={s.repeatBlock}>
+                <img src={repeat} alt={"img"}/>
+            </div>
         </div>
     </div>)
 }
